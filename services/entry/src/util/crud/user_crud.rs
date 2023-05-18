0bf70eb65c97fb3607031c4b8dd330c1014b2344
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2023-05-17 22:56:59 -0700

    simplifying traits for collections.

--- services/entry/src/util/crud/user_crud.rs
+++ services/entry/src/util/crud/user_crud.rs
@@ -1,6 +1,11 @@
 use std::pin::Pin;
 use std::future::Future;
 use anyhow::{Error};
+use super::crud::{AsyncCrud};
+use crate::util::{
+    privilege::privilege::{AsyncPrivilege},
+    indices::index::{AsyncIndices}
+};
 
 pub trait AsyncUserCrud<T> {
     fn create_my(user_id : &str, value: T) 
@@ -19,4 +24,28 @@ pub trait AsyncUserCrud<T> {
         user_id : &str,
         id : &str
     ) -> Pin<Box<dyn Future<Output = Result<T, Error>>>>;
+}
+
+pub const CREATE : &str = "CREATE";
+pub const READ : &str = "READ";
+pub const UPDATE : &str = "UPDATE";
+pub const DELTE : &str = "DELETE";
+
+impl <T, I> AsyncUserCrud<T> for I
+    where
+        I : AsyncCrud<T> + AsyncPrivilege + AsyncIndices 
+{
+
+    fn create_my(user_id : &str, value: T) 
+        -> Pin<Box<dyn Future<Output = Result<T, Error>>>> {
+        
+        Box::pin(async move {
+
+            let val = Self::create(value).await?;
+            Self::
+
+        })
+
+    }
+
 }
--- services/entry/src/util/privilege/privilege.rs
+++ services/entry/src/util/privilege/privilege.rs
@@ -3,10 +3,10 @@ use async_trait::async_trait;
 #[async_trait]
 pub trait AsyncPrivilege {
 
-    async fn add(user_id : &str, privilege_id : &str, resource_id : &str)
+    async fn add_privilege(user_id : &str, privilege_id : &str, resource_id : &str)
     ->Result<bool, Box<dyn std::error::Error>>;
     
-    async fn check(user_id : &str, privilege_id : &str, resource_id : &str)
+    async fn check_privilege(user_id : &str, privilege_id : &str, resource_id : &str)
     ->Result<bool, Box<dyn std::error::Error>>;
 
 }
