0fcf10926d4d808c2204febc17c613e6e39f83f7
Liam Makena Monninger <liam@ramate.io>
2025-08-13 08:54:24 +0100

    feat: add organizational standard structure and tooling.

--- bin/openapi/gen-sendgrid-rs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-openapi-generator generate -g rust \
-  -i https://raw.githubusercontent.com/sendgrid/sendgrid-oai/main/oai_v3_stoplight.json \
-  -o packages/sendgrid-rs \
-  --additional-properties=useSingleRequestParameter=true
--- bin/openapi/gen-twilio-rs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-openapi-generator generate -g rust \
-  -i https://raw.githubusercontent.com/twilio/twilio-oai/main/spec/json/twilio_api_v2010.json \
-  -o packages/twilio-rs \
-  --additional-properties=useSingleRequestParameter=true
